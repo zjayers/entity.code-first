690188acfd98d579dc7266ad7cd1834f6ac46278
Zach Ayers <ayers.dev@icloud.com>
2020-10-15 09:20:47 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // Class to be used to generate Database tables
+    public class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    // Setup DB Context to tell Entity Framework to generate tables for the given classes
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
